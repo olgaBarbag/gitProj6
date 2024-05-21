cded98261ac920a6ba987077441e8b6b8b6466d0
Olga B. <olbarbag@gmail.com>
2024-05-21 21:10:04 +0300

    Last change

--- src/cf/Hello.java
+++ src/cf/Hello.java
@@ -3,6 +3,6 @@ package cf;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!!!!!");
+        System.out.println("Hello World!@#");
     }
 }
